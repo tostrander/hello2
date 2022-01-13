2c97695e65fe4b0733cb4d28752da9f9acaad2c1
Tina Ostrander <tostrander@greenriver.edu>
2022-01-13 17:36:15 -0500

    Home page view added

--- index.php
+++ index.php
@@ -12,7 +12,10 @@ $f3 = Base::instance();
 
 //Define a default route
 $f3->route('GET /', function() {
-    echo "<h1>Hello, World!</h1>";
+    //echo "<h1>Hello, World!</h1>";
+
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //Run fat-free
